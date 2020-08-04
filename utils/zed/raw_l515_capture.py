afefd791190e452623dcb82ab8fd4d7717e0eba1
Alvin Sun <alvinsunyixiao@gmail.com>
2020-08-04 14:09:35 -0500

    add zed l515 raw capture utils

--- utils/zed/raw_l515_capture.py
+++ utils/zed/raw_l515_capture.py
@@ -6,6 +6,7 @@ import argparse
 import cv2
 import numpy as np
 import os
+import pyrealsense2 as rs
 
 from utils.zed.calib import RESOLUTIONS
 
@@ -29,30 +30,41 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(args.camera)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, img_size_wh[0] * 2)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, img_size_wh[1])
-    cap.set(cv2.CAP_PROP_FPS, 60)
+    cap.set(cv2.CAP_PROP_FPS, 30)
     if args.gain is not None:
         cap.set(cv2.CAP_PROP_GAIN, args.gain)
     if args.brightness is not None:
         cap.set(cv2.CAP_PROP_BRIGHTNESS, args.brightness)
-    left_dir = os.path.join(args.output, 'left')
-    right_dir = os.path.join(args.output, 'right')
-    os.makedirs(left_dir, exist_ok=True)
-    os.makedirs(right_dir, exist_ok=True)
+    zed_dir = os.path.join(args.output, 'zed')
+    l515_dir = os.path.join(args.output, 'l515')
+    os.makedirs(zed_dir, exist_ok=True)
+    os.makedirs(l515_dir, exist_ok=True)
+    # setup l515
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+    profile = pipeline.start(config)
     try:
         cnt = 0
         while True:
+            # zed frame
             ret, img = cap.read()
             assert ret, 'camera not returning image'
-            img_left = img[:, :img_size_wh[0], :]
-            img_right = img[:, img_size_wh[0]:, :]
-            cv2.imshow('left', img_left)
-            cv2.imshow('right', img_right)
+            img_zed = img[:, :img_size_wh[0], :]
+            # l515 framee
+            frames = pipeline.wait_for_frames()
+            color_frame = frames.get_color_frame()
+            img_l515 = np.asanyarray(color_frame.get_data())
+            # show image
+            cv2.imshow('zed', img_zed)
+            cv2.imshow('l515', img_l515)
             key = cv2.waitKey(1)
             if key == ord('q'):
                 break
             if key == ord('c'):
-                cv2.imwrite(os.path.join(left_dir, '{}.png'.format(cnt)), img_left)
-                cv2.imwrite(os.path.join(right_dir, '{}.png'.format(cnt)), img_right)
+                cv2.imwrite(os.path.join(zed_dir, '{}.png'.format(cnt)), img_zed)
+                cv2.imwrite(os.path.join(l515_dir, '{}.png'.format(cnt)), img_l515)
                 cnt += 1
     finally:
         cap.release()
